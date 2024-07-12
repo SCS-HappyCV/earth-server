66129b13a002ab6f075c9bc90180481950d58b5e
Bit0r <nie_wang@outlook.com>
2024-07-13 02:05:53 +0800

    feat: 更新2d分割sql代码

--- app/queries/2d_segmentations.sql
+++ app/queries/2d_segmentations.sql
@@ -1,24 +1,32 @@
 -- :name get_2d_segmentation_by_project_id :one
-SELECT *
-FROM 2d_segmentations
-WHERE project_id = :project_id;
+SELECT
+	2d_segmentations.*,
+	p.name,
+	p.created_time,
+	p.updated_time,
+	p.cover_image_id,
+	p.modified_time,
+	p.status
+FROM 2d_segmentations AS 2d_seg, projects AS p
+WHERE
+	2d_seg.project_id = :project_id
+	AND 2d_seg.project_id = p.id
+	AND p.deleted = false;
 
 -- :name get_2d_segmentation :one
 SELECT
-	2d_segmentations.id,
-	image_id,
-	mask_image_id,
-	plot_image_id,
-	result,
-	projects.name AS project_name,
-	projects.created_time AS project_created_time,
-	projects.updated_time AS project_updated_time,
-	projects.cover_image_id
-FROM 2d_segmentations, projects
+	2d_segmentations.*,
+	p.name,
+	p.created_time,
+	p.updated_time,
+	p.cover_image_id,
+	p.modified_time,
+	p.status
+FROM 2d_segmentations AS 2d_seg, projects AS p
 WHERE
-	2d_segmentations.id = :id
-	AND 2d_segmentations.project_id = projects.id
-	AND projects.deleted = false;
+	2d_seg.id = :id
+	AND 2d_seg.project_id = p.id
+	AND p.deleted = false;
 
 -- :name create_2d_segmentation :insert
 INSERT INTO 2d_segmentations (
